dba887fbcbfd11096beb0d71668b25988423eebd
zabrodsk <dusan.zabrodsky@gmail.com>
2025-08-11 11:17:29 -0700

    Create CommutaAppApp.swift

---
+++ CommutaAppApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+/// Entry point for the Commuta iOS prototype.
+@main
+struct CommutaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
